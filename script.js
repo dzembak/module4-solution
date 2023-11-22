83f34b21b1a3fa4ffa41ddb6a95c0607308c5698
Dzembak <104458615+dzembak@users.noreply.github.com>
2023-11-22 12:02:53 +0100

    Ready to be proved

---
+++ script.js
@@ -0,0 +1,29 @@
+// *************
+// ASSIGNMENT 4
+// *************
+/* What should I see in s Console:
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
+*/
+
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})();
